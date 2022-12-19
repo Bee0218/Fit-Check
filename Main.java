34192c5cfef01ee7671e5e7cab856e509102e9b5
Woosung Kim <90729927+Bee0218@users.noreply.github.com>
2022-12-19 13:03:12 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,79 @@
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
         
-        // create add/remove clothes (jacket, top, and bottom)
+        Scanner in = new Scanner(System.in);
+        System.out.println("--------------------------------------------");
+        System.out.println("\tWelcome to the FitCheck app!");
+        System.out.println("--------------------------------------------");
+        System.out.println("Please choose from the following options:");
+        System.out.println("1. Add a new item to your closet");
+        System.out.println("2. Remove an item from your closet");
+        System.out.println("3. View your wardrobe");
+        System.out.println("4. View your outfit");
+        System.out.println("5. Exit the app");
+        System.out.println("--------------------------------------------");
+        System.out.println("Please enter your choice: ");
+        int choice = in.nextInt();
+        
 
-        // create function that returns the outfit combination
+        // while loop to keep the program running until the user wants to exit
+        while ( choice != 5 ) {
+            
+
+            // switch statement to determine which option the user has chosen
+            switch (choice) {
+                case 1:
+                    // add a new item to the wardrobe
+                    // ask user what category of clothing they want to add
+                    System.out.println("What category of clothing would you like to add?");
+                    String category = in.nextLine();
+                    // ask user what colour the clothing is
+                    System.out.println("What colour is the clothing?");
+                    String colour = in.nextLine();
+                    // ask user what the name of the clothing is
+                    System.out.println("What is the name of the clothing?");
+                    String name = in.nextLine();
+
+                    // ask user which closet they want to add the clothing to
+                    System.out.println("Which closet would you like to add the clothing to? \n Winter, Summer, Fall, Spring");
+                    String closet = in.nextLine();
+
+                    
+
+
+                    break;
+                case 2:
+                    // remove an item from the wardrobe
+                    break;
+                case 3:
+                    // view the wardrobe
+                    break;
+                case 4:
+                    // view the outfit
+                    break;
+                default:
+                    // if the user enters an invalid option
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+            }
+            
+            
+            // create a menu for the user to choose from
+            System.out.println("--------------------------------------------");
+            System.out.println("Please choose from the following options:");
+            System.out.println("1. Add a new item to your wardrobe");
+            System.out.println("2. Remove an item from your wardrobe");
+            System.out.println("3. View your wardrobe");
+            System.out.println("4. View your outfit");
+            System.out.println("5. Exit the app");
+            System.out.println("--------------------------------------------");
+            System.out.println("Please enter your choice: ");
+            choice = in.nextInt();
+        }
+
 
     }
 
