6f4e0172c2b8543f53dfe51781b05807fd4a3aa7
Woosung Kim <90729927+Bee0218@users.noreply.github.com>
2022-12-19 12:23:59 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // create add/remove clothes (jacket, top, and bottom)
+
+        // create function that returns the outfit combination
+
+    }
+
+}
